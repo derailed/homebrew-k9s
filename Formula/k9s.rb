e0ce69de699f284ab2a14aa6667dbbec975c4361
derailed <fernand@imhotep.io>
2019-02-08 15:02:28 -0700

    Brew formula update for k9s version 0.1.3

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -1,9 +1,9 @@
 class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9ss.io"
-  url "https://github.com/derailed/k9s/releases/download/0.1.2/k9s_0.1.2_Darwin_x86_64.tar.gz"
-  version "0.1.2"
-  sha256 "7f1dcd288571a9f185fb4f2da1da20596175d95dc4053f07b54714a5b68d669c"
+  url "https://github.com/derailed/k9s/releases/download/0.1.3/k9s_0.1.3_Darwin_x86_64.tar.gz"
+  version "0.1.3"
+  sha256 "8cbd1d38c3927cbf36275ff936aa2ceef1a67f651f131cb10450d866a8135179"
 
   def install
     bin.install "k9s"
