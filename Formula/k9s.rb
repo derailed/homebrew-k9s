f11e4a6601603fdabbd08767052c7aa391b44a84
derailed <fernand@imhotep.io>
2019-03-25 19:07:49 -0600

    Brew formula update for k9s version 0.3.2

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -3,7 +3,7 @@ class K9s < Formula
   homepage "https://k9ss.io"
   url "https://github.com/derailed/k9s/releases/download/0.3.2/k9s_0.3.2_Darwin_x86_64.tar.gz"
   version "0.3.2"
-  sha256 "07ee9aa2bb9bd4fce85832eabbf357d432950e18cae455f011ba313451c98346"
+  sha256 "7311899c553eb93fa3f6fd92bfc6105189d6c74b2fe86fac193bb5e7937c4830"
 
   def install
     bin.install "k9s"
