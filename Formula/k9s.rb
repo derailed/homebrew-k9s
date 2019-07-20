ceb526d0f1647a6e29161c8f4edf83f21c5e96a0
derailed <fernand.galiana@gmail.com>
2019-07-19 19:39:43 -0600

    add linux

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -1,9 +1,15 @@
 class K9s < Formula
   desc "A CLI tool to manage your Kubernetes clusters"
   homepage "https://k9ss.io"
-  url "https://github.com/derailed/k9s/releases/download/0.1.0/k9s_0.1.1_Darwin_x86_64.tar.gz"
   version "0.1.0"
+
+  if os.Mac?
+  url "https://github.com/derailed/k9s/releases/download/0.1.0/k9s_0.1.1_Darwin_x86_64.tar.gz"
   sha256 "ad07db979cc0f66bdf0442d6df6eaa58aade347ba0b280b4c6b551fe481d22ae"
+  elsif os.Linux?
+    url "https://github.com/derailed/k9s/releases/download/0.1.0/k9s_0.1.1_Linux_x86_64.tar.gz"
+    sha256 "0d4ef92c2da1aa73e8da60aeae1ae0004bcf42e5e1ed8d982ea0fca8b8633ebe"
+  end
 
   def install
     bin.install "k9s"
