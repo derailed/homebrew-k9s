4555ea73a46b0602943b3d7bfb4869009fc72b74
derailed <fernand.galiana@gmail.com>
2019-07-19 20:16:29 -0600

    updates

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -3,14 +3,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9ss.io"
   version "0.7.2"
-
-  if os.Mac?
   url "https://github.com/derailed/k9s/releases/download/0.7.12/k9s_0.7.12_Darwin_x86_64.tar.gz"
   sha256 "5275223cb1efee19d97cc63e46e461fbde34993f6af43e3f1ff2c4bd63bb6a35"
-  elsif os.Linux?
-    url "https://github.com/derailed/k9s/releases/download/0.7.12/k9s_0.7.12_Linux_x86_64.tar.gz"
-    sha256 "0d4ef92c2da1aa73e8da60aeae1ae0004bcf42e5e1ed8d982ea0fca8b8633ebe"
-  end
 
   def install
     bin.install "k9s"
