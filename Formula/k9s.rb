f6cd5ccfc624dc2d243e8f62ba5b4aa51f9bc04d
derailed <fernand@imhotep.io>
2020-01-23 15:38:35 -0700

    Brew formula update for k9s version v0.13.4

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,24 +2,24 @@
 class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k8sk9s.dev/"
-  version "0.13.3"
+  version "0.13.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/derailed/k9s/releases/download/v0.13.3/k9s_0.13.3_Darwin_x86_64.tar.gz"
-    sha256 "379f6e16c889637e8c99158bd1ac6898514de4ee7898c3e4fee3b94ad914fc45"
+    url "https://github.com/derailed/k9s/releases/download/v0.13.4/k9s_0.13.4_Darwin_x86_64.tar.gz"
+    sha256 "954d8f65a37526eb3f0df14e9c06ae1a9280b0c00d281e7bee8c6d4563d42486"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/derailed/k9s/releases/download/v0.13.3/k9s_0.13.3_Linux_x86_64.tar.gz"
-      sha256 "864f95477c21c845da656b405fe51cdeb6015e85d5755b76dae1dd1112d3f906"
+      url "https://github.com/derailed/k9s/releases/download/v0.13.4/k9s_0.13.4_Linux_x86_64.tar.gz"
+      sha256 "6c8502723ee624d83e7ec98e96334f83fa17b491d3fa5c4cfb97b022e790d3fe"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/derailed/k9s/releases/download/v0.13.3/k9s_0.13.3_Linux_arm64.tar.gz"
-        sha256 "3bc451f82cc001a13ff18266845f5e251a7b9884a44052545d86db1ee5bc4d45"
+        url "https://github.com/derailed/k9s/releases/download/v0.13.4/k9s_0.13.4_Linux_arm64.tar.gz"
+        sha256 "34eafda836a91aeb51c390c31e7e3ecb2a52956cd61afe353a2041d6df73072f"
       else
-        url "https://github.com/derailed/k9s/releases/download/v0.13.3/k9s_0.13.3_Linux_armv6.tar.gz"
-        sha256 "dc80abc007e1317e982dc22e6bc5e30a351eac777c831b4758cc5a6e0e623df2"
+        url "https://github.com/derailed/k9s/releases/download/v0.13.4/k9s_0.13.4_Linux_armv6.tar.gz"
+        sha256 "aed1ce97a9a7766ddcdd89095dfbbee234e7e002efdbf50a6f3f2e043373f81e"
       end
     end
   end
