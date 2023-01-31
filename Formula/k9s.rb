7ce114e2d3a2b57cc3bda9ffab287ed74a02377d
derailed <fernand@imhotep.io>
2023-01-31 10:37:51 -0700

    Brew formula update for k9s version v0.27.2

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -5,54 +5,42 @@
 class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
-  version "0.27.1"
+  version "0.27.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/derailed/k9s/releases/download/v0.27.1/k9s_Darwin_arm64.tar.gz"
-      sha256 "4ee3c3c4af47c63be1bd4c876add16051aad94f733c4f405ea813efdbdd4eebb"
+      url "https://github.com/derailed/k9s/releases/download/v0.27.2/k9s_Darwin_arm64.tar.gz"
+      sha256 "13e71b63da6f198758ffc5840191d7a4d9f116457e4d6f06d60164ae436f6c64"
 
       def install
-        system "make", "build"
-        bin.install "execs/k9s"
-
-        generate_completions_from_executable(bin/"k9s", "completion")
+        bin.install "k9s"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/derailed/k9s/releases/download/v0.27.1/k9s_Darwin_amd64.tar.gz"
-      sha256 "dd21770bc26eaf08392c8ce892cd5be335b55778d8616ff8a8f863157138b81a"
+      url "https://github.com/derailed/k9s/releases/download/v0.27.2/k9s_Darwin_amd64.tar.gz"
+      sha256 "aac311825f7d90f20eb2769d7da28d3fa59ceb48652c2615dd630f77b41eff98"
 
       def install
-        system "make", "build"
-        bin.install "execs/k9s"
-
-        generate_completions_from_executable(bin/"k9s", "completion")
+        bin.install "k9s"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/derailed/k9s/releases/download/v0.27.1/k9s_Linux_arm64.tar.gz"
-      sha256 "4086e4fa92324e5462a34cbb44e18c54b714af91af454c3a5f1546028669006f"
+      url "https://github.com/derailed/k9s/releases/download/v0.27.2/k9s_Linux_arm64.tar.gz"
+      sha256 "e745166277c2eab7e1d06fcc3492d01efa6a5a6bd0c77a068f47a49d4d8e7272"
 
       def install
-        system "make", "build"
-        bin.install "execs/k9s"
-
-        generate_completions_from_executable(bin/"k9s", "completion")
+        bin.install "k9s"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/derailed/k9s/releases/download/v0.27.1/k9s_Linux_amd64.tar.gz"
-      sha256 "0b3fca451e8f642fb5682346b9e00e23b23fa372a98a287616baf2e1fb14d795"
+      url "https://github.com/derailed/k9s/releases/download/v0.27.2/k9s_Linux_amd64.tar.gz"
+      sha256 "8d4bfb9bfee8e28300305746002002b27f47badbd19e25a503d38172006e1170"
 
       def install
-        system "make", "build"
-        bin.install "execs/k9s"
-
-        generate_completions_from_executable(bin/"k9s", "completion")
+        bin.install "k9s"
       end
     end
   end
